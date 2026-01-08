194dc35529567e1f2f2bd6f0d400db20830aef6e
iamcheese-man <Ilyassmekarto9@gmail.com>
2026-01-08 23:29:46 +0100

    Update UnifiedScripts.lua

--- UnifiedScripts.lua
+++ UnifiedScripts.lua
@@ -642,16 +642,16 @@ SATKTab:CreateButton({
     end
 })
 
--- #######################
--- #### Adonis Bypass ####
--- #######################
+-- ############################
+-- #### Client Protections ####
+-- ############################
 
-local AdonisTab = Window:CreateTab("Adonis", "shield")
-AdonisTab:CreateSection("Client Protection")
+local CProtectTab = Window:CreateTab("Client Protections", "shield")
+CProtectTab:CreateSection("ADONIS ANTI-CHEAT")
 
-local BypassEnabled = false
+local BypassAdonisEnabled = false
 
-local badFunctions = {
+local badAdonisFunctions = {
     "Crash","CPUCrash","GPUCrash","Shutdown","SoftShutdown",
     "Kick","SoftKick","Seize","BlockInput","Break","Lock",
     "SetCore","ServerKick","ServerShutdown","Ban","Mute",
@@ -666,18 +666,18 @@ end
 local function neutralize(tbl)
     if type(tbl) ~= "table" then return end
     for k,v in pairs(tbl) do
-        if tableFind(badFunctions,k) and type(v)=="function" then
+        if tableFind(badAdonisFunctions,k) and type(v)=="function" then
             tbl[k] = function() warn("[Adonis Blocked]",k) end
         end
     end
 end
 
-AdonisTab:CreateToggle({
+CProtectTab:CreateToggle({
     Name = "Enable Adonis Bypass",
     CurrentValue = false,
     Flag = "Adonis_Enable",
     Callback = function(v)
-        BypassEnabled = v
+        BypassAdonisEnabled = v
         if not v then return end
 
         for _,m in ipairs(getloadedmodules()) do
